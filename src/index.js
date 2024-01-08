723895602daff795b168d9b32939c6dea81e3c4f
ridwanndwip <ridwanndwip@gmail.com>
2024-01-09 02:49:50 +0700

    update footer section

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ const navbar = document.querySelector('.navbar_menu')
 NavToggle.addEventListener('click', function(){
  navbar.classList.toggle('seret');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const stopScrollingCheckbox = document.getElementById("stopScrollingCheckbox");
+
+    stopScrollingCheckbox.addEventListener("change", function() {
+        const body = document.body;
+
+        if (this.checked) {
+            // Disable scrolling
+            body.style.overflowY = "hidden";
+        } else {
+            // Enable scrolling
+            body.style.overflowY = "auto";
+        }
+    });
+});
